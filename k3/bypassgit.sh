0e7c6845c8cdb8e91042e6e7af611d8c9d09d043
Hanz <zwillhill@hotmail.com>
2022-03-09 19:42:20 +0800

    Update bypassgit.sh

--- k3/bypassgit.sh
+++ k3/bypassgit.sh
@@ -17,34 +17,34 @@
 #============================================================
 # Replace k3screenctrl
 rm -rf package/lean/k3screenctrl
-git clone https://github.com/zwillhill/k3screenctrl_build.git package/lwz322/k3screenctrl
+git clone --depth=1 https://github.com/zwillhill/k3screenctrl_build.git package/lwz322/k3screenctrl
 # Add luci-app-k3screenctrl
-git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/lwz322/luci-app-k3screenctrl
+git clone --depth=1 https://github.com/lwz322/luci-app-k3screenctrl.git package/lwz322/luci-app-k3screenctrl
 #============================================================
 # Replace luci-app-dnsfilter
 # rm -rf package/lean/luci-app-dnsfilter
-git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/kiddin9/luci-app-dnsfilter
+git clone --depth=1 https://github.com/kiddin9/luci-app-dnsfilter.git package/kiddin9/luci-app-dnsfilter
 #============================================================
 # Add luci-theme-darkmatter with moded one
 svn co https://github.com/zwillhill/luci-theme-darkmatter/trunk/luci/themes/luci-theme-darkmatter package/apollo-ng/luci-theme-darkmatter
 #============================================================
 # Add luci-app-vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/jerrykuku/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/jerrykuku/luci-app-vssr
+git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git package/jerrykuku/lua-maxminddb
+git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr.git package/jerrykuku/luci-app-vssr
 #============================================================
 # Add luci-app-ssr-plus with depency
 git clone https://github.com/fw876/helloworld package/fw876
 #============================================================
 # Add luci-app-bypass
-git clone https://github.com/kiddin9/openwrt-bypass.git package/kiddin9/openwrt-bypass
+git clone --depth=1 https://github.com/kiddin9/openwrt-bypass.git package/kiddin9/openwrt-bypass
 #============================================================
 # Add passwall with depency
 # git clone https://github.com/xiaorouji/openwrt-passwall package/xiaorouji/openwrt-passwall
 # svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall package/kenzok8/openwrt-passwall
-git clone https://github.com/kenzok8/small-package package/kenzok8
+git clone --depth=1 https://github.com/kenzok8/small-package package/kenzok8
 #============================================================
 # Add OpenClash
-git clone https://github.com/vernesong/OpenClash package/vernesong
+git clone --depth=1 https://github.com/vernesong/OpenClash package/vernesong
 #============================================================
 # Add luci-app-adguardhome
 # svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/kenzok8/luci-app-adguardhome
